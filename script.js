c48ac1feebaebef8290da3c16f402a2f3836895d
Mikail <i.mikail6@gmail.com>
2021-02-04 09:21:05 +0400

    third commit

--- script.js
+++ script.js
@@ -1,36 +1,30 @@
 /* Задание на урок:
 
-1) Автоматизировать вопросы пользователю про фильмы при помощи цикла
+1) Первую часть задания повторить по уроку+
 
-2) Сделать так, чтобы пользователь не мог оставить ответ в виде пустой строки,
-отменить ответ или ввести название фильма длинее, чем 50 символов. Если это происходит - 
-возвращаем пользователя к вопросам опять
+2) Создать функцию showMyDB, которая будет проверять свойство privat. Если стоит в позиции
+false - выводит в консоль главный объект программы
 
-3) При помощи условий проверить  personalMovieDB.count, и если он меньше 10 - вывести сообщение
-"Просмотрено довольно мало фильмов", если от 10 до 30 - "Вы классический зритель", а если больше - 
-"Вы киноман". А если не подошло ни к одному варианту - "Произошла ошибка"
+3) Создать функцию writeYourGenres в которой пользователь будет 3 раза отвечать на вопрос 
+"Ваш любимый жанр под номером ${номер по порядку}". Каждый ответ записывается в массив данных
+genresa,aaaa
 
-4) Потренироваться и переписать цикл еще двумя способами*/
+P.S. Функции вызывать не обязательно*/
 
 'use strict';
 
-/* 1) Создать переменную numberOfFilms и в неё поместить ответ от пользователя на вопрос:
-'Сколько фильмов вы уже посмотрели?' */
 
-let numberOfFilms = prompt('Сколько фильмов вы уже посмотрели','');
+let numberOfFilms;
 
-while (numberOfFilms == false || numberOfFilms.length > 50 || numberOfFilms === null ){
-    numberOfFilms = prompt('Сколько фильмов вы уже посмотрели','');
+function start() {
+    numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели','');
+    
+    while (numberOfFilms == '' || numberOfFilms == null || isNaN(numberOfFilms)) {
+        numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели','');
+    }
 }
 
-
-/* 2) Создать объект personalMovieDB и в него поместить такие свойства:
-    - count - сюда передается ответ на первый вопрос
-    - movies - в это свойство поместить пустой объект
-    - actors - тоже поместить пустой объект
-    - genres - сюда поместить пустой массив
-    - privat - в это свойство поместить boolean(логическое) значение false */
-
+start();
 
 let personalMovieDB = {
 	count : numberOfFilms,
@@ -40,23 +34,46 @@ let personalMovieDB = {
 	privat : false
 };
 
-if (personalMovieDB.count < 10) {
-    alert("Просмотрено довольно мало фильмов");
-} else if ((10 <= personalMovieDB.count && personalMovieDB.count < 30)) {
-    alert("Вы классический зритель");
-} else if (30 <= personalMovieDB.count) {
-    alert("Вы киноман");
-} else {
-    alert("Произошла ошибка");
-}
-
-for (let i = 0; i < 2; i++) {
-    let a = prompt('Один из последних просмотренных фильмов?','');
-    let b = prompt('На сколько оцените его?', '');
-
-    if (a != null && b != null && a != '' && b != '' && a.length < 50) {
-        personalMovieDB.movies[a] = b;
+function rememberMyFilms() {
+    for (let i = 0; i < 2; i++) {
+        let a = prompt('Один из последних просмотренных фильмов?','');
+        let b = prompt('На сколько оцените его?', '');
+    
+        if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+            personalMovieDB.movies[a] = b;
+        }
     }
     
 }
 
+rememberMyFilms();
+
+function detectPersonallevel () {
+    if (personalMovieDB.count < 10) {
+        alert("Просмотрено довольно мало фильмов");
+    } else if ((10 <= personalMovieDB.count && personalMovieDB.count < 30)) {
+        alert("Вы классический зритель");
+    } else if (30 <= personalMovieDB.count) {
+        alert("Вы киноман");
+    } else {
+        alert("Произошла ошибка");
+    }
+}
+
+detectPersonallevel ();
+
+function showMyDB(hidden) {
+    if (!hidden) {
+        console.log(personalMovieDB);
+    } 
+}
+
+showMyDB(personalMovieDB.private);
+
+function writeYourGenres() {
+    for ( let i = 1; i <= 3; i++ ) {
+        personalMovieDB.genres[i - 1] = prompt(`Ваш любимый жанр под номером ${i}`);
+    }
+}
+
+writeYourGenres();
